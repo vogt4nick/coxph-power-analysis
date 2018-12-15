dea0a7fe1ac36765146d64c581ebc2ed4e79df97
Nick.Vogt <nick.vogt@dynatrace.com>
2018-12-15 14:38:30 -0500

    use engineer_features.R

--- src/viz/visualize-effects.R
+++ src/viz/visualize-effects.R
@@ -1,61 +1,13 @@
 
+#' Visualize single effects of each model
+#' The idea here to decide which effects we'll use in the mixed model
+
 library(tidyverse)
-library(broom)
-library(dotwhisker)
 
 
 load('models/explore-effects.Rdata')
 load('data/interim/correctness.RData')
-
-#' Engineer Features from dataset parameters
-engineer_features <- function(dat) {
-  dat %>% 
-    mutate(
-      # ePeriodsBaseline = log(0.5) / log(1 - baselineHazardRate),
-      # expectedLifetimes = (maxPeriods / ePeriodsBaseline),
-      # nClosedEnrollmentPeriods = maxPeriods - maxCohorts,
-      # nOpenEnrollmentPeriods = maxPeriods - nClosedEnrollmentPeriods,
-      # pctOpenEnrollmentPeriods = nOpenEnrollmentPeriods / maxPeriods,
-      baselineHazardRate2 = baselineHazardRate ** 2,
-      baselineHazardRate3 = baselineHazardRate ** 3,
-      logBaselineHazardRate = log(baselineHazardRate),
-      treatmentHazardRatio2 = treatmentHazardRatio **2,
-      treatmentHazardRatio3 = treatmentHazardRatio ** 3,
-      logTreatmentHazardRatio = log(treatmentHazardRatio),
-      cohortSize2 = cohortSize ** 2,
-      cohortSize3 = cohortSize ** 3,
-      logCohortSize = log(cohortSize),
-      expectedLifetimes2 = expectedLifetimes ** 2,
-      expectedLifetimes3 = expectedLifetimes ** 3,
-      logExpectedLifetimes = log(expectedLifetimes),
-      pctOpenEnrollmentPeriods2 = pctOpenEnrollmentPeriods ** 2,
-      pctOpenEnrollmentPeriods3 = pctOpenEnrollmentPeriods ** 3,
-      logPctOpenEnrollmentPeriods = log(pctOpenEnrollmentPeriods)
-    )
-}
-
-# datapoints <- correctness %>% engineer_features()
-
-# Dot-Whisker -------------------------------------------------------------
-
-m1 <- tidy(linear_effects) %>% 
-  filter(term != "(Intercept)") %>% 
-  mutate(model = "Linear")
-
-dwplot(
-    m1,
-    vline = geom_vline(xintercept = 0, colour = "grey60", linetype = 2)
-  ) %>%
-  relabel_predictors(c(
-     'baselineHazardRate' = 'Baseline Hazard',
-     'treatementHazardRatio' = 'Treatment HR',
-     'cohortSize' = 'Cohort Size',
-     'expectedLifetimes' = 'Expected Lifetimes',
-     'pctOpenEnrollmentPeriods' = '% Open Enrollment'
-  )) + 
-  theme_minimal()
-
-
+source('src/features/engineer-features.R')
 
 
 # Baseline Hazard ---------------------------------------------------------
